beca78cb22e114cc10db6f6642c9ce13d5957fba
subedib0 <subedib0@gmail.com>
2018-02-22 17:02:50 -0500

    with question

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
 import java.util.Scanner;
+// How Much water runs off a roof in a rain storm?
+//To calculate the runoff from any given rainfall: Take the dimensions of the footprint of the rooof and convert them to inches. (so a 50'*20' roof is 600"*240). Multiply the roof dimensions by
+//the number of inches of rainfall. in this example, 600"*240"*1"=144,000 cubic inches of water. Devid that result by 231 to get the number of gallons (because
+//1 gallon= 231 cubic inches). (144,000/231=623.38).
+//create a new project called RainfallCalculator. This will be a console application like those you've looked at so far.
+//Implement your algorithm for calculating the amount of rainfall that runs off my roof. Prompt the user to enter the length and width of the roof along with any other veriables you need.
+//your solution must Implement atleast one method.
+
+
+
+
 
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
+//defining veriable
         double rainFall;
         boolean result = true;
         double roofLength;
@@ -18,9 +29,10 @@ public class Main {
         double nGallon;
         System.out.println(header());
 
-
+ //loop to find the amount of different rainfall
         while (result) {
 //            break;
+
             System.out.println("Enter a length of roof :");
             roofLength = scan.nextDouble();
             scan.nextLine();
